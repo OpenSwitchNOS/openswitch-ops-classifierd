50775f26ad0421d4eb11db9eca4e262d6ad9d14b
Sandip Bidaye <sandip.bidaye@hpe.com>
2016-04-20 10:52:07 -0700

    Convert entry_id to sequence_no for status msg
    This commit has following changes
    - acl_port.c, convert entry_id to sequence no
      using key_cur_aces
    - acl.c, convert entry_id to sequence no
      using key_in_progress_aces
    
    Change-Id: Ib8f7bd601154adc3c6060b1925e1e1075f1e948c
    Signed-off-by: Sandip Bidaye <sandip.bidaye@hpe.com>

--- src/acl/switchd-plugin/acl.c
+++ src/acl/switchd-plugin/acl.c
@@ -368,6 +368,7 @@ acl_update_internal(struct acl* acl)
 
     char details[256];
     char status_str[OPS_CLS_STATUS_MSG_MAX_LEN] = {0};
+    unsigned int sequence_number = 0;
     struct ops_cls_list *list = ops_cls_list_new_from_acl(acl);
     if (!list) {
         sprintf(details, "ACL %s -- unable to translate from ovsdb",
@@ -405,15 +406,17 @@ acl_update_internal(struct acl* acl)
                     " acl entry = %u and port = %s", acl->name,
                      status.entry_id, netdev_get_name(status.port->netdev));
             VLOG_DBG(details);
-            /* @todo entry_id needs to be converted to sequence no here
-             * but that infra structure is not ready yet.
-             */
+            /* Convert entry_id to sequence_number using in_progress aces */
+            if(status.entry_id < acl->ovsdb_row->n_in_progress_aces) {
+                sequence_number =
+                    acl->ovsdb_row->key_in_progress_aces[status.entry_id];
+            }
             ops_cls_status_msgs_get(status.status_code,
                                     OPS_CLS_STATUS_MSG_OP_UPDATE_STR,
                                     OPS_CLS_STATUS_MSG_FEATURE_ACL_STR,
                                     OPS_CLS_STATUS_MSG_IFACE_PORT_STR,
                                     netdev_get_name(status.port->netdev),
-                                    status.entry_id,
+                                    sequence_number,
                                     OPS_CLS_STATUS_MSG_MAX_LEN,status_str);
             acl_set_cfg_status(acl->ovsdb_row, ACL_CFG_STATE_REJECTED,
                                status.status_code, status_str);
