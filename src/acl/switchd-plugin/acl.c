04f3bd3b28c1c5f352283a3c01e05c322220695f
Sandip Bidaye <sandip.bidaye@hpe.com>
2016-04-20 10:52:07 -0700

    Convert entry_id to sequence_no for status msg
    This commit has following changes
    - acl_port.c, convert entry_id to sequence no
      using key_cur_aces
    - acl.c, convert entry_id to sequence no
      using key_in_progress_aces
    
    Change-Id: Ib8f7bd601154adc3c6060b1925e1e1075f1e948c
    Signed-off-by: Sandip Bidaye <sandip.bidaye@hpe.com>

--- src/acl/switchd-plugin/acl.c
+++ src/acl/switchd-plugin/acl.c
@@ -413,7 +413,8 @@ acl_update_internal(struct acl* acl)
                                     OPS_CLS_STATUS_MSG_FEATURE_ACL_STR,
                                     OPS_CLS_STATUS_MSG_IFACE_PORT_STR,
                                     netdev_get_name(status.port->netdev),
-                                    status.entry_id,
+                                    acl->ovsdb_row->
+                                        key_in_progress_aces[status.entry_id],
                                     OPS_CLS_STATUS_MSG_MAX_LEN,status_str);
             acl_set_cfg_status(acl->ovsdb_row, ACL_CFG_STATE_REJECTED,
                                status.status_code, status_str);
