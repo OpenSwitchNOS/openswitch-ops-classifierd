75de29da95c2d959f059d3e84212a4a283d3eae8
Nachiket Abhyankar <nabhyank@hpe.com>
2016-05-02 16:47:41 -0700

    chg: dev: acl delete after applying to a port
    This commit adds the ability to delete an ACL after
    applying it to a port. All applications are unapplied
    and the access list is deleted from config
    
    Change-Id: I06c24fea48cea544b3151033c440bc49926204e8
    Signed-off-by: Nachiket Abhyankar <nabhyank@hpe.com>

--- src/acl/switchd-plugin/acl.c
+++ src/acl/switchd-plugin/acl.c
@@ -18,6 +18,7 @@
 #include "sort.h"
 #include "smap.h"
 #include "json.h"
+#include "list.h"
 #include "vswitch-idl.h"
 #include "openvswitch/vlog.h"
 #include "ofproto/ofproto-provider.h"
@@ -26,6 +27,7 @@
 #include "acl_ofproto.h"
 #include "reconfigure-blocks.h"
 #include "acl_plugin.h"
+#include "acl_port.h"
 #include "ops_cls_status_msgs.h"
 
 VLOG_DEFINE_THIS_MODULE(acl_switchd_plugin_global);
@@ -463,10 +465,7 @@ acl_cfg_delete(struct acl* acl)
 {
     VLOG_DBG("ACL %s deleted", acl->name);
 
-    /* Unapply ACL on any ports is handled as part of
-       plugin reconfigure blocks.
-       TODO: Check if there are any ports left and report error if the p2acl list is not empty*/
-
+    acl_port_unapply_if_needed(acl);
     acl_delete(acl);
 }
 
