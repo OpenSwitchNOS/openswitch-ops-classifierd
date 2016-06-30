7ee55bc5f6aaccc23309bdbc8b7ef948e5221795
Gary Wassermann <gary.wassermann@hpe.com>
2016-06-30 09:12:03 -0700

    Update classifierd daemon to support egress IPv4 ACLs
    Change-Id: Ifdad57ced3019dcdc5089e25e00a6a28b95547e8
    Signed-off-by: Gary Wassermann <gary.wassermann@hpe.com>

--- src/acl/daemon/acl_daemon_init.c
+++ src/acl/daemon/acl_daemon_init.c
@@ -38,6 +38,7 @@ acl_ovsdb_init(struct ovsdb_idl *idl)
     /* Monitor the following columns, marking them read-only. */
     ovsdb_idl_add_column(idl, &ovsrec_port_col_name);
     ovsdb_idl_add_column(idl, &ovsrec_port_col_aclv4_in_status);
+    ovsdb_idl_add_column(idl, &ovsrec_port_col_aclv4_out_status);
     /* @todo: we may need a column to monitor per interface ACL
      * applied status, especially for LAGs
      */
