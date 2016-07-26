307c74687ceed186a895becafdca11d892155534
Vivek Mehrotra <vivek.mehrotra@hpe.com>
2016-07-26 14:29:38 -0700

    chg: dev: Add log/count support when configuring ACL
    Change-Id: I41464123cb0ba1184a0fa0d8634c6be6f709b689
    Signed-off-by: Vivek Mehrotra <vivek.mehrotra@hpe.com>

--- ops-tests/feature/acl_classifier_common_lib.py
+++ ops-tests/feature/acl_classifier_common_lib.py
@@ -20,6 +20,7 @@ from re import search, findall
 from topology_lib_scapy.library import ScapyThread, send_traffic, sniff_traffic
 from time import sleep
 import re
+from ipdb import set_trace
 
 
 """
@@ -48,18 +49,36 @@ Use Case for Library Functions:
 
 def configure_acl(
             switch1, acl_name, seq_num, action, proto, src_ip,
-            src_port, dst_ip, dst_port, count_str
+            src_port, dst_ip, dst_port, *count_log_str
         ):
     """
     Configure an ACL with one permit or one deny rule
     """
 
+    count_str = ''
+    log_str = ''
+    display_cnt = 0
+    display_str = ''
+
+    for arg in count_log_str:
+        if arg == 'count':
+            count_str = 'count'
+            display_cnt = display_cnt + 1
+            display_str = 'count'
+        elif arg == 'log':
+            log_str = 'log'
+            display_cnt = display_cnt + 1
+            display_str = 'log'
+    if display_cnt == 2:
+        display_str = 'log'
+
+    set_trace()
     if action == "permit":
         with switch1.libs.vtysh.ConfigAccessListIpTestname(acl_name) as ctx:
             ctx.permit(
                       '',
                       seq_num, proto, src_ip, src_port,
-                      dst_ip, dst_port, count_str
+                      dst_ip, dst_port, count_str, log_str
                       )
 
     elif action == "deny":
@@ -67,17 +86,25 @@ def configure_acl(
             ctx.deny(
                       '',
                       seq_num, proto, src_ip, src_port,
-                      dst_ip, dst_port, count_str
+                      dst_ip, dst_port, count_str, log_str
                       )
     else:
         assert(False)
 
+    action_line_args = [seq_num, action, proto, src_ip, src_port,
+                        dst_ip, dst_port, display_str]
+
+    action_line_str = ' '.join(args for args in action_line_args)
+    print('action_line_str is {}'.format(action_line_str))
+    """
     action_line_str = (
                         seq_num + ' ' + action + ' ' + proto
                         + ' ' + src_ip + ' ' + src_port +
                         ' ' + dst_ip + ' ' + dst_port +
-                        ' ' + count_str
+                        ' ' + count_str + ' ' + log_str
                     )
+    """
+
     action_line_str = re.sub('\s+', ' ', action_line_str).strip()
 
     action_line_re = re.compile(action_line_str)
@@ -106,7 +133,22 @@ def apply_acl(switch1, port_num, acl_name, direction):
                     )
 
     test_result = switch1('show run')
+
+    if direction == 'in':
+        interface_info, rest, *misc = test_result.split(
+                        'apply access-list ip test in'
+                                    )
+    else:
+        interface_info, rest, *misc = test_result.split(
+                        'apply access-list ip test out'
+                                    )
+    interface_line = findall(r'interface\s+\d+', interface_info)[-1]
+    interface_num = search('(?<=interface )\d+', interface_line).group()
+    print('interface is {}'.format(interface_num))
+    # assert (interface_num == port_num)
+
     assert re.search(apply_line_re, test_result)
+    return interface_num
 
 
 def no_acl(switch1, acl_name):
